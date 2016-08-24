c990c3df1fb9805f45dccb55dbc4b50801646904
aifs24 <mathbrain24@hotmail.com>
2016-08-24 16:21:16 -0700

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name munchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the munchApp
+ */
+angular.module('munchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name munchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the munchApp
+ */
+angular.module('munchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
