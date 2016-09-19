0365ca4a304021be9536c8a5bc30c52e0689f4d5
aifs24 <mathbrain24@hotmail.com>
2016-09-18 22:47:31 -0700

    Resultados de api en results

--- app/scripts/controllers/result.js
+++ app/scripts/controllers/result.js
@@ -8,10 +8,14 @@
  * Controller of the munchApp
  */
 angular.module('munchApp')
-  .controller('ResultCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ResultCtrl', function($scope, $http) {
+    $scope.request = function() {
+      $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269")
+        .success(function (data) {
+          $scope.datos = data.report.foods;
+        })
+        .error(function (error) {
+          console.log(error);
+        });
+    }
   });
