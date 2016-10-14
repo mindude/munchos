31f533f308f45f86c2f0daff374f8f8f064a2692
mindude <eleal@cetys.edu.mx>
2016-10-14 16:58:47 -0700

    Modified main view format

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the munchApp
  */
 angular.module('munchApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $html, $routeProvider) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.SaltBtn = function(){
+      $location.path( "/result" );
+    }
   });
