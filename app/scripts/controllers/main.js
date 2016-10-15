3ab0962e3fc0c5f44210e41f5813ad5aa74f7fd4
mindude <eleal@cetys.edu.mx>
2016-10-14 20:54:54 -0700

    Updated main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,25 @@
  * Controller of the munchApp
  */
 angular.module('munchApp')
-  .controller('MainCtrl', function ($scope, $html, $routeProvider) {
+  .controller('MainCtrl', function ($scope, $location) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $scope.SaltBtn = function(){
-      $location.path( "/result" );
+      //$location.path('/result');
+    }
+    $scope.SourBtn = function(){
+      //$location.path('/result');
+    }
+    $scope.BitterBtn = function(){
+      //$location.path('/result');
+    }
+    $scope.SweetBtn = function(){
+      //$location.path('/result');
+    }
+    $scope.SpicyBtn = function(){
+      //$location.path('/result');
     }
   });
