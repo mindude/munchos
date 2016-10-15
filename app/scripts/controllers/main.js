e7b58cf59f1dd8c4cad52b1cd715a02ecce5ada1
mindude <eleal@cetys.edu.mx>
2016-10-14 22:56:34 -0700

    Added background image and logo icon

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,18 +15,18 @@ angular.module('munchApp')
       'Karma'
     ];
     $scope.SaltBtn = function(){
-      //$location.path('/result');
-    }
+      $location.path('/result');
+    };
     $scope.SourBtn = function(){
-      //$location.path('/result');
-    }
+      $location.path('/result');
+    };
     $scope.BitterBtn = function(){
-      //$location.path('/result');
-    }
+      $location.path('/result');
+    };
     $scope.SweetBtn = function(){
-      //$location.path('/result');
-    }
-    $scope.SpicyBtn = function(){
-      //$location.path('/result');
-    }
+      $location.path('/result');
+    };
+    $scope.SavoryBtn = function(){
+      $location.path('/result');
+    };
   });
