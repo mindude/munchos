aa6e5853a09f436be4493985845609a31cba3676
mindude <eleal@cetys.edu.mx>
2016-12-07 20:39:09 -0800

    Eliminated unnecesary views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,30 +24,10 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/healthy', {
-        templateUrl: 'views/healthy.html',
-        controller: 'HealthyCtrl',
-        controllerAs: 'healthy'
-      })
       .when('/result', {
         templateUrl: 'views/result.html',
         controller: 'ResultCtrl',
         controllerAs: 'result'
-      })
-      .when('/test', {
-        templateUrl: 'views/test.html',
-        controller: 'TestCtrl',
-        controllerAs: 'test'
-      })
-      .when('/GPSView', {
-        templateUrl: 'views/gpsview.html',
-        controller: 'GpsviewCtrl',
-        controllerAs: 'GPSView'
       })
       .otherwise({
         redirectTo: '/'
